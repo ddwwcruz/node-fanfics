e2e5bb7512f8f2cb51e1409866c67aff63d34887
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-04 20:55:44 +0800

    Register getAllChapters in index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import _getInfo from './get-info'
 import _getChapter from './get-chapter'
+import _getAllChapters from './get-all-chapters'
 
 export const getInfo = _getInfo
 export const getChapter = _getChapter
+export const getAllChapters = _getAllChapters
