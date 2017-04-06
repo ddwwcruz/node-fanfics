2de61802d652d6812ac9fd035cc2731730f21797
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-06 20:43:33 +0800

    Add function for getting author information

--- src/get-author-info.ts
+++ src/get-author-info.ts
@@ -6,3 +6,56 @@ export interface AuthorInformation {
     profileText: string
     imgUrl: string
 }
+
+function buildInfo(args: Document): AuthorInformation {
+    var name = args.querySelector('#content_wrapper_inner span').textContent
+    var profileTextSelector = args.querySelector('#bio')
+        .querySelectorAll('div, p, hr')
+    var profileText = ''
+    var imgUrl = null
+
+    for (let a = 0; a < profileTextSelector.length; a++) {
+        profileText += profileTextSelector.item(a).outerHTML
+    }
+
+    try {
+        imgUrl = args.querySelector('#bio img').getAttribute('src')
+    } catch (err) {
+        // do nothing
+    }
+
+    return {
+        name,
+        profileText,
+        imgUrl
+    }
+}
+
+/**
+ * Get the information of an author
+ * @param id ID of the author
+ */
+export async function getAuthorInfo(id: number) {
+    var url = `https://www.fanfiction.net/u/${id}`
+    return new Promise<AuthorInformation>((resolve, reject) => {
+        get(url, (err, res, body) => {
+            if (err) {
+                reject(err)
+            } else if (res && res.statusCode == 200) {
+                try {
+                    env(body, (err, win) => {
+                        if (err) reject(err)
+                        else resolve(buildInfo(win.document))
+                    })
+                } catch (err) {
+                    reject(err)
+                }
+            } else if (res && res.statusCode != 200) {
+                reject({
+                    code: res.statusCode,
+                    message: res.statusMessage
+                })
+            }
+        })
+    })
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { getInfo } from './get-info'
 export { getChapter } from './get-chapter'
 export { getAllChapters } from './get-all-chapters';
+export { getAuthorInfo } from './get-author-info';
