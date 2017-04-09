16c216e345f3c06bec34d9b2fa9e7fc3c20f813f
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-10 00:16:20 +0800

    Story should also have an id tag

--- src/story.ts
+++ src/story.ts
@@ -1,5 +1,6 @@
 export default class Story {
     constructor(
+        public readonly id: number,
         public readonly title: string,
         public readonly authorName: string,
         public readonly summary: string,
